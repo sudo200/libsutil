60b6f989be4d398a720c7c8c8662a60349071fba
sudo200 <65343982+sudo200@users.noreply.github.com>
2022-10-12 19:33:21 +0200

    Comment dmem.h and types.h

--- include/dmem.h
+++ include/dmem.h
@@ -3,9 +3,22 @@
 
 #include "types.h"
 
+/**
+ * Function pointer to the function used to allocate memory dynamically.
+ */
 extern alloc_t ualloc;
+
+/**
+ * Function pointer to the function used to deallocate dynamically allocated memory.
+ */
 extern dealloc_t ufree;
+
+/**
+ * Funtion pointer to the function used to resize dynamically allocated memory.
+ */ 
 extern realloc_t urealloc;
+
+
 
 #endif//___DMEM_H__
 
--- include/types.h
+++ include/types.h
@@ -5,16 +5,51 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+/**
+ * Datatype of file descriptors.
+ */
 typedef int fd_t;
+
+/**
+ * Datatype of the errno variable.
+ */
 typedef int errno_t;
 
+
+/**
+ * A function getting nothing and returning nothing.
+ * Often used as a callback.
+ */
 typedef void (*callback_t)(void);
+
+/**
+ * A function executed in a posix thread.
+ */
 typedef void * (*thread_worker_t)(void *);
+
+/**
+ * A callback function for a signal.
+ */
 typedef void (*sighandler_t)(int);
+
+/**
+ * A function for allocating memory dynamically.
+ */
 typedef void * (*alloc_t)(size_t);
+
+/**
+ * A function for deallocating dynamic memory.
+ */
 typedef void (*dealloc_t)(void *);
+
+/**
+ * A function for resizing a dynamic memory area.
+ */
 typedef void * (*realloc_t)(void *, size_t);
 
+/**
+ * A struct containing an array of strings and its length.
+ */
 typedef struct
 {
   char **arr;
