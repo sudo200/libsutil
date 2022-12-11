f046f23024babfc66ce6efe60e199e6e87d9a689
sudo200 <65343982+sudo200@users.noreply.github.com>
2022-12-11 17:50:03 +0100

    Refactor linkedlist test

--- test/linkedlist_test.c
+++ test/linkedlist_test.c
@@ -11,56 +11,136 @@
 static int i = 0;
 
 void cb_function(void *item, void *pipe) {
-  test("foreach cb_function equals",
-       EQUALS((const char *)item, ((char **)pipe)[i++]));
+  assert(EQUALS((const char *)item, ((char **)pipe)[i++]));
 }
 
-int main(void) {
-  char *strs[] = {"foo", "bar", "boom", "saas", "sees", "soos"};
+char *strs[] = {"foo", "bar", "boom", "saas", "sees", "soos"};
 
-  linkedlist *list = linkedlist_new();
-  test("new != NULL", list != NULL);
+linkedlist *list;
 
-  test("empty length", linkedlist_length(list) == 0);
-  test("empty element", linkedlist_get(list, 0) == NULL);
+void add_NULL(void) {
+  ASSERT(linkedlist_add(NULL, ""));
+}
 
-  test("add 0", linkedlist_add(list, strs[0]) >= 0);
+void addall_NULL(void) {
+  ASSERT(linkedlist_addall(NULL, NULL, 3));
+}
 
-  test("foreach exec", linkedlist_foreach(list, cb_function, strs) >= 0);
-  i = 0;
+void new_non_NULL(void) {
+  list = linkedlist_new();
+  ASSERT(list != NULL);
+}
 
-  test("add 1", linkedlist_add(list, strs[1]) >= 0);
-  test("add 2", linkedlist_add(list, strs[2]) >= 0);
+void empty_after_creation(void) {
+  assert(linkedlist_length(list) == 0);
+  ASSERT(linkedlist_get(list, 0) == NULL);
+}
 
-  test("add 3", linkedlist_length(list) == 3);
+void add_0(void) {
+  int ret = linkedlist_add(list, strs[0]);
+  ASSERT(ret >= 0);
+}
 
-  test("add rest", linkedlist_addall(list, (void **)(strs + 3), 3) >= 0);
+void add_1(void) {
+  int ret = linkedlist_add(list, strs[1]);
+  ASSERT(ret >= 0);
+}
 
-  test("list[1] == strs[1]", EQUALS(linkedlist_get(list, 1), strs[1]));
-  test("list[5] == strs[5]", EQUALS(linkedlist_get(list, 5), strs[5]));
-  test("foreach exec", linkedlist_foreach(list, cb_function, strs) >= 0);
+void add_2(void) {
+  int ret = linkedlist_add(list, strs[2]);
+  ASSERT(ret >= 0);
+}
 
-  void **arr = linkedlist_to_array(list);
-  test("to_array not NULL", arr != NULL);
-  test("to_array equal",
-       MEMEQUALS(arr, strs, sizeof(*arr) * linkedlist_length(list)));
+void check_after_add(void) {
+  assert(linkedlist_length(list) == 3);
+  ASSERT(linkedlist_get(list, 3) == NULL);
+}
+
+void addall(void) {
+  int ret = linkedlist_addall(list, (void **)(strs + 3), 3);
+  ASSERT(ret >= 0);
+}
+
+void confirm_length(void) {
+  ASSERT(linkedlist_length(list) == 6);
+}
+
+void **arr;
+
+void to_array_not_NULL(void) {
+  arr = linkedlist_to_array(list);
+  ASSERT(arr != NULL);
+}
+
+void to_array_equal(void) {
+  ASSERT(MEMEQUALS(arr, strs, sizeof(*arr) * linkedlist_length(list)));
   ufree(arr);
+}
 
-  test("length == 6", linkedlist_length(list) == 6);
+void foreach_0(void) {
+  int ret = linkedlist_foreach(list, cb_function, strs);
+  ASSERT(ret >= 0);
+}
 
-  test("clear exec", linkedlist_clear(list) >= 0);
+void remove_5(void) {
+  void *item = linkedlist_remove(list, 5);
+  ASSERT(EQUALS(item, strs[5]));
+}
 
-  test("add 5", linkedlist_add(list, strs[5]) >= 0);
-  test("insert (1-4) @ 0",
-       linkedlist_insertall(list, (void **)strs + 1, 4, 0) >= 0);
-  test("insert 0 @ 0", linkedlist_insert(list, strs[0], 0) >= 0);
+void remove_0(void) {
+  void *item = linkedlist_remove(list, 0);
+  ASSERT(EQUALS(item, strs[0]));
+}
 
-  test("insert 0 @ 0", linkedlist_insert(list, strs[0], 0) >= 0);
-  test("remove 0", EQUALS((const char *)linkedlist_remove(list, 0), strs[0]));
+void foreach_1(void) {
+  i = 1;
+  int ret = linkedlist_foreach(list, cb_function, strs);
+  ASSERT(ret >= 0);
+}
 
+void insert(void) {
+  int ret = linkedlist_insert(list, strs[0], 0);
+  ASSERT(ret >= 0);
+}
+
+void foreach_2(void) {
   i = 0;
-  test("foreach exec", linkedlist_foreach(list, cb_function, strs) >= 0);
+  int ret = linkedlist_foreach(list, cb_function, strs);
+  ASSERT(ret >= 0);
+}
 
+void clear(void) {
+  int ret = linkedlist_clear(list);
+  ASSERT(ret >= 0);
+}
+
+void empty_after_clear(void) {
+  assert(linkedlist_length(list) == 0);
+  ASSERT(linkedlist_get(list, 0) == NULL);
+}
+
+
+int main(void) {
+  RUN_TEST(add_NULL);
+  RUN_TEST(addall_NULL);
+  RUN_TEST(new_non_NULL);
+  RUN_TEST(empty_after_creation);
+  RUN_TEST(add_0);
+  RUN_TEST(add_1);
+  RUN_TEST(add_2);
+  RUN_TEST(check_after_add);
+  RUN_TEST(addall);
+  RUN_TEST(confirm_length);
+  RUN_TEST(to_array_not_NULL);
+  RUN_TEST(to_array_equal);
+  RUN_TEST(foreach_0);
+  RUN_TEST(remove_5);
+  RUN_TEST(remove_0);
+  RUN_TEST(foreach_1);
+  RUN_TEST(insert);
+  RUN_TEST(foreach_2);
+  RUN_TEST(clear);
+  RUN_TEST(empty_after_clear);
   linkedlist_destroy(list);
   return 0;
 }
