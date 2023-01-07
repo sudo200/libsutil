13db946bab8645260e49dc4c0f422f2320e0fbb6
sudo200 <65343982+sudo200@users.noreply.github.com>
2023-01-07 17:19:15 +0100

    Add executability

--- src/globals.c
+++ src/globals.c
@@ -1,3 +1,27 @@
 #include "globals.h"
 
 const char *const LIBRARY_VERSION = HEADER_VERSION;
+
+#define INFOPAGE                                                               \
+  "libsutil " HEADER_VERSION "\n"                                              \
+  "================================\n"                                         \
+  "This is sudo200's utility library,\n"                                       \
+  "originally developed just for fun, it\n"                                    \
+  "kind of became a kitchen-sink library\n"                                    \
+  "for everything from argument parsing,\n"                                    \
+  "file handling to entire data structures.\n"
+
+/////////////////////////////////////////////////////////////
+///              WARNING! Black magic below!              ///
+/////////////////////////////////////////////////////////////
+#include <sys/unistd.h>
+
+// Sadly, system dependant :(
+#define __LD "/lib/ld-linux-x86-64.so.2"
+
+const char ld_loader[] __attribute__((section(".interp"))) = __LD;
+
+__attribute__((force_align_arg_pointer)) void _start(void) {
+  write(1, INFOPAGE, sizeof(INFOPAGE) - 1);
+  _exit(0);
+}
